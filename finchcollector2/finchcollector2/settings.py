7c1377c125e5f47e34e74d3b4edee0b04d19a89e
BrianTaylor <bataylor135@gmail.com>
2024-02-05 22:22:48 -0500

    readme update

--- finchcollector2/finchcollector2/settings.py
+++ finchcollector2/finchcollector2/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'finch',
+           'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
